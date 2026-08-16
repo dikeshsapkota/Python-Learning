8865842b8ca6273158ba8bf93110106aed578a6d
Dikesh Sapkota <dikeshsapkota@gmail.com>
2026-08-16 15:27:59 +0545

    day1 final

--- python-basics/Day-01/exercise.py
+++ python-basics/Day-01/exercise.py
@@ -4,8 +4,8 @@ price = 1000
 discount_percentage = 10
 def calculate_discount(price, discount_percentage):
     
-    calculate_discount=price-(price*discount_percentage/100)
-    return calculate_discount
+   final_price=price-(price*discount_percentage/100)
+   return final_price
 print(f" original price {price} and discount percentage {discount_percentage} ")
 
 print(f"final price {calculate_discount(price, discount_percentage)}")
@@ -17,6 +17,7 @@ products = [
     {"name": "Keyboard", "price": 3000, "stock": 10},
     {"name": "Monitor", "price": 25000, "stock": 0},
 ]
+total_inventory_value = 0
 for product in products:
     if product["price"]>2000:
         print(product["name"])
@@ -24,7 +25,15 @@ for product in products:
 for product in products:
     if product["stock"] > 0:
         print (f"in Stock {product["name"]} at price {product["price"]}")
+        total_stock_price=product["price"] * product["stock"]
+        total_inventory_value+=total_stock_price
+        
     else:
         print (f"Out of Stock {product["name"]}")
 
+ #final exercise day 1:
  
+print(f"total value is {total_inventory_value}")
+
+
+   
