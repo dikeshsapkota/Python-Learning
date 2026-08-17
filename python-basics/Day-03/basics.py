2342c1c2ddde2a9d6d2fe1246ba7b6da1b579ab9
Dikesh Sapkota <dikeshsapkota@gmail.com>
2026-08-17 12:22:56 +0545

    File handling and Json in python

--- python-basics/Day-03/basics.py
+++ python-basics/Day-03/basics.py
@@ -5,4 +5,29 @@ try:
 except ValueError:
     print("enter valid number")      
 
-#2    
+#2 File Handling
+with open("test.txt","w")as file: #opens the file and automatically closes it afterward.
+    file.write("Learning Python")
+""""r" → read
+"w" → write/overwrite
+"a" → append"""
+with open("test.txt", "r") as file:
+    content = file.read()
+
+print(content)
+
+#3 JSON
+import json
+product = {
+    "name": "Laptop",
+    "price": 80000,
+    "stock": 5
+}
+#write json file
+with open("product.json", "w") as file:
+    json.dump(product, file, indent=4)
+#read json file
+with open("product.json", "r") as file:
+    loaded_product = json.load(file)
+
+print(loaded_product)
---
+++ python-basics/Day-03/exercise.py
@@ -0,0 +1,26 @@
+"""products
+   ↓
+save to products.json
+   ↓
+read products.json
+   ↓
+loop through loaded products
+   ↓
+print"""
+import json
+products = [
+    {"name": "Laptop", "price": 80000, "stock": 5},
+    {"name": "Mouse", "price": 1500, "stock": 10},
+    {"name": "Keyboard", "price": 3000, "stock": 7}
+]
+with open("product.json", "w") as file:
+    json.dump(products, file, indent=4)
+with open("product.json", "r") as file:
+    loaded_product = json.load(file)
+for product in products:
+    print(f"{product["name"]}:{product["price"]}:{product["stock"]}")
+ #mini challenege
+with open("learning_log.txt","w")as file:
+    file.write(f"Day 1 - Python fundamentals\n"
+f"Day 2 - Functions and validation\n"
+f"Day 3 - Exceptions, files and JSON")
