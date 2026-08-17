ae94f42049a519d7f9b6a2476436d220c9c1ab8d
Dikesh Sapkota <dikeshsapkota@gmail.com>
2026-08-17 18:11:42 +0545

    Day-04 class exercise

--- python-basics/Day-04/exercise.py
+++ python-basics/Day-04/exercise.py
@@ -1,1 +1,17 @@
 #1
+class Product:
+    def __init__(self, name, price, stock):
+        self.name = name
+        self.price = price
+        self.stock = stock
+    def total_stock_value(self):
+        return self.price * self.stock
+    def is_in_stock(self):
+     return (self.stock>0)
+            
+
+laptop = Product("Laptop", 80000, 5)
+print(laptop.is_in_stock())
+mouse = Product("Mouse", 1500, 0)
+
+print(mouse.is_in_stock())
