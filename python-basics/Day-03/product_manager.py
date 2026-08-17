7e6ce84d44502ced6ac3a8e9b35d461091ff5598
Dikesh Sapkota <dikeshsapkota@gmail.com>
2026-08-17 14:00:07 +0545

    feat: complete Day 3 JSON product manager

--- python-basics/Day-03/product_manager.py
+++ python-basics/Day-03/product_manager.py
@@ -3,10 +3,7 @@ with open("product.json", "r") as file:
    products = json.load(file)
 print(products)
 for product in products:
-    print(product["name"])
-    print(product["price"])
-    print(product["stock"])
-
+    print(f"{product["name"]}|{product["price"]}|{product["stock"]}" )
 
 add_name=input("enter product name")
 add_price=int(input("enter price"))
@@ -16,8 +13,18 @@ new_product= {
     "price":add_price,
     "stock":add_stock
 }
-
 products.append(new_product)
+with open("product.json", "w") as file:
+    json.dump(products, file, indent=4)
 
+give_name=input("enter name to be deleted")
+found =False
+for product in products:
+    if(give_name==product["name"]):
+        products.remove(product)
+        found=True
+        break   
+if not found:
+    print("invalid name")    
 with open("product.json", "w") as file:
     json.dump(products, file, indent=4)
