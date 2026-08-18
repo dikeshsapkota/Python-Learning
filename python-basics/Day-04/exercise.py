ff01af5fb3f6ca2d5437046053053cb894f57184
Dikesh Sapkota <dikeshsapkota@gmail.com>
2026-08-18 14:44:31 +0545

    Day-04product summary

--- python-basics/Day-04/exercise.py
+++ python-basics/Day-04/exercise.py
@@ -7,11 +7,14 @@ class Product:
     def total_stock_value(self):
         return self.price * self.stock
     def is_in_stock(self):
-     return (self.stock>0)
-            
+        return (self.stock>0)
+    def get_summary(self):
+      print(f"{self.name}| {self.price}| {self.stock}")
+                
 
 laptop = Product("Laptop", 80000, 5)
 print(laptop.is_in_stock())
 mouse = Product("Mouse", 1500, 0)
 
 print(mouse.is_in_stock())
+laptop.get_summary()
